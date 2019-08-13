a15d6ce65ebe1ce1791d003141391d2081bb07a4
Tim Bodiford <tcbodiford@gmail.com>
2019-08-13 15:23:14 -0400

    started server for first time - resolved issues with viewsets

--- dogood_app/urls.py
+++ dogood_app/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path, include
+from rest_framework import routers
+
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('organizations', views.OrganizationView)
+router.register('events', views.EventView)
+router.register('volunteers', views.VolunteerView)
+
 urlpatterns = [
+    path('', include(router.urls))
+
 ]
--- dogood_app/views.py
+++ dogood_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import EventSerializer, OrganizationSerializer, VolunteerSerializer
+from .models import Organization, Event, Volunteer
+
 
 # Create your views here.
+
+class OrganizationView(viewsets.ModelViewSet):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+
+class EventView(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class VolunteerView(viewsets.ModelViewSet):
+    queryset = Volunteer.objects.all()
+    serializer_class = VolunteerSerializer
