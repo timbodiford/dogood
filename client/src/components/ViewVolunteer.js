3b5381cbbe47766c0a731c67025a8f12d7007aa6
Tim Bodiford <tcbodiford@gmail.com>
2019-08-16 14:44:36 -0400

    able to delete a volunteer

--- client/src/components/ViewVolunteer.js
+++ client/src/components/ViewVolunteer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
+
 
 export default class ViewVolunteer extends Component {
 
@@ -14,15 +16,25 @@ export default class ViewVolunteer extends Component {
 
     getVol = () => {
         axios.get(`/api/v1/volunteers/${this.props.match.params.volId}/`)
-        .then((res) => {
-            this.setState({ volunteer: res.data })
-        })
+            .then((res) => {
+                this.setState({ volunteer: res.data })
+            })
+    }
+
+    handleDelete = () => {
+        axios.delete(`/api/v1/volunteers/${this.state.volunteer.id}/`)
+            .then(() => {
+                this.setState({ redirectToHome: true })
+            })
     }
 
     render() {
+        if (this.state.redirectToHome) {
+            return <Redirect to={`/volunteers`} />
+        }
         return (
             <div>
-                    <h1>This component should show a single volunteer</h1>
+                <h1>This component should show a single volunteer</h1>
                 <h4>{this.state.volunteer.name}</h4>
                 <p>{this.state.volunteer.address}</p>
                 <p>{this.state.volunteer.city}</p>
@@ -32,6 +44,9 @@ export default class ViewVolunteer extends Component {
                 <Link to="/">
                     <button type="button">Back to Home</button>
                 </Link>
+                <div>
+                    <button onClick={this.handleDelete}>Delete This Volunteer</button>
+                </div>
 
             </div>
         )
